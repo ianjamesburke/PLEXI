5ee357e88c70b06e54e21ce8caaec468ef98af07
ianjamesburke <37986506+ianjamesburke@users.noreply.github.com>
2026-05-15 00:01:05 -0400

    feat(website): add /install redirect to raw install script

---
+++ website/src/pages/install.ts
@@ -0,0 +1,11 @@
+export const prerender = false;
+
+export async function GET() {
+  return new Response(null, {
+    status: 302,
+    headers: {
+      Location:
+        "https://raw.githubusercontent.com/ianjamesburke/PLEXI/main/scripts/user-install.sh",
+    },
+  });
+}
